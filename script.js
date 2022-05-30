20ee1d3a0701942c12b2e0b1da8d851eb221e53b
SPARTACUS5329 <adityaspartacus5329@gmail.com>
2022-05-30 16:38:42 +0530

    finished proj0 of api call

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
 $(document).ready(function () {
-  var fontSize = 30;
-  $("form").on("submit", function () {
-    var input = $("#new-item");
-    var todo = { item: input.val() };
-    var divId = todo.item.split(" ").join("_");
-    var liElement = `<div id = "${divId}"><li>${todo.item}</li></div>`;
-    input.val("");
-    $("#todo-list").append(liElement);
-    $(`#${divId} li`).on("click", function () {
-      var divId = $(this).text().split(" ").join("_");
-      $(`#${divId}`).remove();
-    });
-    return false;
-  });
-
-  $("#reduce-font").click(function () {
-    fontSize = Math.max(fontSize - 3, 3);
-    document.querySelector("textarea").style.fontSize = `${fontSize}px`;
-  });
-  $("#increase-font").click(function () {
-    fontSize = Math.min(fontSize + 3, 60);
-    document.querySelector("textarea").style.fontSize = `${fontSize}px`;
+  $.ajax({
+    type: "GET",
+    url: "https://cat-fact.herokuapp.com/facts",
+    success: function (data) {
+      let factContainer;
+      data.forEach((fact, index) => {
+        factContainer = $(`#fact-${index + 1}`);
+        factContainer.text(fact.text);
+      }, this);
+    },
+    error: function (error) {
+      $("#error-text").text(error.message);
+      console.error(error);
+    },
   });
 });
